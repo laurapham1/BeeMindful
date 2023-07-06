3274f2b50d7a82f6a0c765672284840fc856747c
Laura Pham <l.pham2311@gmail.com>
2023-07-06 15:18:43 +1000

    add emotion buttons

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bee Mindful App 🐝</Text>
+      <View collapsable="true" style={styles.emotionsContainer}>
+        {/* emotion buttons which shows an overlay on click */}
+      <Button onPress={() => console.log('hi')} title="😡" accessibilityLabel='angry' color="white"/>
+      <Button onPress={() => console.log('hi')} title="🙁" accessibilityLabel='sad' color="white"/>
+      <Button onPress={() => console.log('hi')} title="😐" accessibilityLabel='neutral' color="white"/>
+      <Button onPress={() => console.log('hi')} title="😊" accessibilityLabel='content' color="white"/>
+      <Button onPress={() => console.log('hi')} title="😄" accessibilityLabel='joyful' color="white" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +25,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  emotionsContainer: {
+    flexDirection: 'row',
+  }
+})
