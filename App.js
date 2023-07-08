fac9cc196f0ab0da5a16b4bd3c75458b1e1a733a
Laura Pham <l.pham2311@gmail.com>
2023-07-08 23:58:36 +1000

    add tab navigator to app

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './views/HomeScreen';
 import ProfileScreen from './views/ProfileScreen';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Tab.Navigator initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: '#e91e63',
+      }}>
+        <Tab.Screen
           name="Home"
           component={HomeScreen}
           options={{title: 'BeeMindful Home'}}
         />
-        <Stack.Screen name="Profile" component={ProfileScreen}/>
-      </Stack.Navigator>
+        <Tab.Screen name="Profile" component={ProfileScreen} options={{
+          tabBarLabel: 'Profile',
+          // tabBarIcon: ({ color, size }) => (
+          //   <MaterialCommunityIcons name="home" color={color} size={size} />
+          // ),
+        }}/>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
