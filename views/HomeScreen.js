de45bb7c08b7e9d82926f92658d45ab666c94523
Laura Pham <l.pham2311@gmail.com>
2023-07-14 16:15:18 +1000

    update home page with additional journal prompts

--- views/HomeScreen.js
+++ views/HomeScreen.js
@@ -1,21 +1,34 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Modal, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Modal, TouchableOpacity, TextInput } from 'react-native';
 import { primaryColor } from '../helpers/themes';
 
 export default function HomeScreen({navigation}) {
   const [modalVisible, setModalVisible] = useState(false)
+  const [selectedEmotions, setSelectedEmotions] = useState(false)
+  const [thoughts, setThoughts] = useState('')
+  const [actions, setActions] = useState('')
+
+  const handlePressEmotion = () => {
+    // add selected emotion to selected emotion array
+    setModalVisible(!modalVisible)
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>BeeMindful 🐝</Text>
-      <Text style={styles.heading}>How are you feeling today?</Text>
+      <Text style={{...styles.heading, fontWeight: '600', marginBottom: '36px'}}>How are you today?</Text>
+      <Text style={{...styles.heading, fontSize: '20px'}}>My feelings</Text>
       <View collapsable="true" style={styles.emotionsContainer}>
         {/* emotion buttons which shows an overlay on click */}
-          <TouchableOpacity onPress={() => setModalVisible(!modalVisible)} accessibilityLabel='angry' color="white" ><Text style={styles.emotionContainer}>😡</Text></TouchableOpacity>
-          <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}  accessibilityLabel='sad' color="white" ><Text style={styles.emotionContainer}>🙁</Text></TouchableOpacity>
-          <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}  accessibilityLabel='neutral' color="white" ><Text style={styles.emotionContainer}>😐</Text></TouchableOpacity>
-          <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}  accessibilityLabel='content' color="white" ><Text style={styles.emotionContainer}>😊</Text></TouchableOpacity>
-          <TouchableOpacity onPress={() => setModalVisible(!modalVisible)} accessibilityLabel='joyful' color="white" ><Text style={styles.emotionContainer}>😄</Text></TouchableOpacity>
+          <TouchableOpacity onPress={() => handlePressEmotion()} accessibilityLabel='angry' color="white" ><Text style={styles.emotionContainer}>😡</Text></TouchableOpacity>
+          <TouchableOpacity onPress={() => handlePressEmotion()}  accessibilityLabel='sad' color="white" ><Text style={styles.emotionContainer}>🙁</Text></TouchableOpacity>
+          <TouchableOpacity onPress={() => handlePressEmotion()}  accessibilityLabel='neutral' color="white" ><Text style={styles.emotionContainer}>😐</Text></TouchableOpacity>
+          <TouchableOpacity onPress={() => handlePressEmotion()}  accessibilityLabel='content' color="white" ><Text style={styles.emotionContainer}>😊</Text></TouchableOpacity>
+          <TouchableOpacity onPress={() => handlePressEmotion()} accessibilityLabel='joyful' color="white" ><Text style={styles.emotionContainer}>😄</Text></TouchableOpacity>
       </View>
+      <Text style={{...styles.heading, fontSize: '20px'}}>My thoughts</Text>
+      <TextInput editable multiline numberOfLines={6} onChangeText={text => setThoughts(text)} value={thoughts} style={styles.inputArea}/>
+      <Text style={{...styles.heading, fontSize: '20px'}}>My actions</Text>
+      <TextInput editable multiline numberOfLines={6} onChangeText={text => setActions(text)} value={actions} style={styles.inputArea}/>
       <Modal
         animationType="slide"
         transparent={true}
@@ -46,18 +59,19 @@ const styles = StyleSheet.create({
     backgroundColor: primaryColor,
     alignItems: 'center',
     justifyContent: 'center',
-    gap: '24px'
+    gap: '24px',
   },
   heading: {
-    fontSize: '24px',
-    color: 'white'
+    fontSize: '30px',
+    color: 'white',
+    fontWeight: '350'
   },
   emotionsContainer: {
     flexDirection: 'row',
     gap: '16px'
   },
   emotionContainer: {
-    fontSize: '10vw',
+    fontSize: '36px',
   },
   centeredView: {
     flex: 1,
@@ -90,7 +104,14 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     padding: 10,
     elevation: 2,
-    backgroundColor: 'white !important',
+    backgroundColor: 'white',
     border: '1px solid black',
   },
+  inputArea: {
+    backgroundColor: 'white',
+    borderRadius: '8px',
+    padding: '8px',
+    fontSize: '16px',
+    width: '75vw'
+  }
 })
